53ef7053b54f75be3a92b46a066063d6e3a0af8f
vilish <vilish.kumar@cscglobal.com>
2020-07-02 08:18:06 -0400

    Insert in a sorted Array

--- src/main/java/org/example/icamp/chap05/binarysearch/InsertTargetInSortedArr.java
+++ src/main/java/org/example/icamp/chap05/binarysearch/InsertTargetInSortedArr.java
@@ -3,29 +3,27 @@ package org.example.icamp.chap05.binarysearch;
 public class InsertTargetInSortedArr {
 
     public int insertAt(int[] a, int target) {
-
         if (a == null) return -1;
 
         int low = 0, high = a.length - 1;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            //if target is less than or equal to mid element
-            if (target >= a[mid]) {
+            //if target is less than  mid element
+            if (target < a[mid]) {
 
-                //if mid is hi end, return hi+1
-                if (mid == high) {
-                    return high + 1;
-                }
-                low = mid + 1;       // move right
-            } else {  // when target is less than mid element
-
-                //if mid is lo end, return lo
+                //if mid is hi end, return mid index
                 if (mid == low) {
                     return mid;
                 }
-
                 high = mid - 1;       // move left
+            } else {  // when target is more than or equal to mid element
+
+                //if mid is high end, return next index
+                if (mid == high) {
+                    return mid + 1;
+                }
+                low = mid + 1;       // move right
             }
 
         }
@@ -33,7 +31,4 @@ public class InsertTargetInSortedArr {
         return low;  // empty array
     }
 
-    private int calculateDiff(int[] a, int mid, int target, int currentDiff) {
-        return Math.min(a[mid] - target, currentDiff);
-    }
 }
